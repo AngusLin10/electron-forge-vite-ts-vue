3b5ef5d2ac0f395f6272c327b730c447c1676a47
Angus Lin <Angus.lin@microtek.com.tw>
2024-01-30 11:42:31 +0800

    initial version

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+</template>
+
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
