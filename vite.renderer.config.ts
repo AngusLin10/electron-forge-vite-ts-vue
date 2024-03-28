0122db000364e9c0e30a743874d3684014155419
Angus Lin <Angus.lin@microtek.com.tw>
2024-03-28 11:08:22 +0800

    modify config file to support element-plus

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,6 +2,9 @@ import type { ConfigEnv, UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import { pluginExposeRenderer } from './vite.base.config';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -16,7 +19,16 @@ export default defineConfig((env) => {
     build: {
       outDir: `.vite/renderer/${name}`,
     },
-    plugins: [pluginExposeRenderer(name), vue()],
+    plugins: [
+      pluginExposeRenderer(name),
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
     resolve: {
       preserveSymlinks: true,
     },
